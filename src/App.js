8b3847451c79d8bdd0f8c890452fcf4dd0664ad0
Max <max@webmaestro.com.ua>
2024-04-28 16:50:45 +0300

    adds mint form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Minter from './Minter'
+import Lake from './Lake'
+
+function App() {
+  return (
+    <div className="App">
+      <Lake/>
+      <Minter></Minter>
+    </div>
+  );
+}
+
+export default App;
