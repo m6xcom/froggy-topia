8d19f9d3a37a69b5bd97ba7846c02329db37261f
Max <max@webmaestro.com.ua>
2024-04-29 21:42:16 +0300

    adds eth balance

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import "./styles/index.css";
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
 import lineaData from './utils/eip155-59141.json';
 
-const chains:any[] = [lineaData.chainId]!;
-
 const projectId:string = process.env.WALLET_CONNECT_PROJECT_ID!;
 
 const network = {
@@ -16,6 +14,8 @@ const network = {
 	explorerUrl: lineaData.explorers[0].url,
 	rpcUrl: lineaData.rpc[0]
   }
+
+const chains:any[] = [network]!;
 
 const metadata = {
 	name: "Froggy Topia",
@@ -38,8 +38,11 @@ createWeb3Modal({
 	includeWalletIds: ['c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
 		'4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
 	],
+	featuredWalletIds: ['c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+		'4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+	],
 	tokens: {
-		1: {
+		[lineaData.chainId]: {
 		  address: '0x06565ed324Ee9fb4DB0FF80B7eDbE4Cb007555a3',
 		  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png"
 		},
