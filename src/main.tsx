3a4ae0e45700abde447f941b89aedfb6896be241
Max <max@webmaestro.com.ua>
2024-04-29 21:25:26 +0300

    fixes typescript

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import "./styles/index.css";
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
 import lineaData from './utils/eip155-59141.json';
 
-const chains = [lineaData.chainId];
+const chains:any[] = [lineaData.chainId]!;
 
-const projectId = process.env.WALLET_CONNECT_PROJECT_ID;
+const projectId:string = process.env.WALLET_CONNECT_PROJECT_ID!;
 
 const network = {
 	chainId: lineaData.chainId,
@@ -19,6 +19,7 @@ const network = {
 
 const metadata = {
 	name: "Froggy Topia",
+	description: 'Froggy Topia app',
 	url: "https://web3modal.com",
 	icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
@@ -33,7 +34,6 @@ createWeb3Modal({
 		'--w3m-color-mix': '#88AA11',
 		'--w3m-color-mix-strength': 40
 	},
-	allowUnsupportedChain: true,
 	defaultChain: network,
 	includeWalletIds: ['c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
 		'4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
